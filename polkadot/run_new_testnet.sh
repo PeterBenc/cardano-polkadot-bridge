24088d9217c13967f0c9b5a00193745b54029112
Peter Benc <peter.benc1@gmail.com>
2022-03-13 22:03:34 +0100

    Add git keeps

--- polkadot/run_new_testnet.sh
+++ polkadot/run_new_testnet.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Removing old polkadot files"
-rm -r ./docker/polkadot/.local
+rm -r ../docker/polkadot/.local
 
-polkadot_local_testnet_config_path="../snowbridge/parachain/config.json"
+polkadot_local_testnet_config_path="./config.json"
 
 echo "Starting polkadot local network"
 polkadot-launch $polkadot_local_testnet_config_path
