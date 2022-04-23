e1239d7cf1d76494d60613a7075b55c55dee9b80
Peter Benc <peter.benc1@gmail.com>
2022-04-23 17:33:56 +0200

    Generate parachain spec when launching local network

--- polkadot/run_local_testnet.sh
+++ polkadot/run_local_testnet.sh
@@ -4,5 +4,8 @@ rm -r ../docker/polkadot/.local
 
 polkadot_local_testnet_config_path="./polkadot-launch-config.json"
 
+# build spec for the parachain
+../parachain/target/release/snowbridge build-spec --chain snowbase --disable-default-bootnode > spec.json
+
 echo "Starting polkadot local network"
 polkadot-launch $polkadot_local_testnet_config_path
